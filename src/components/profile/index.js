b8172cd3d77a70c612d8faa26e9a473b93311c6e
Marcos Moreira <marcosmoreira.r@gmail.com>
2022-01-11 21:00:58 -0300

    primeiro commit projeto

---
+++ src/components/profile/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+import * as S from "./styled";
+
+const Profile = () => {
+  return (
+    <S.Wrapper>
+      <S.WrapperImage
+        src="https://avatars.githubusercontent.com/u/48301925?v=4"
+        alt="Avatar of user"
+      />
+      <S.WrapperInfoUser>
+        <div>
+          <h1>Marcos Moreira</h1>
+          <S.WrapperUserName>
+            <h3>Username: </h3>
+            <span>marcosrmoreira</span>
+          </S.WrapperUserName>
+        </div>
+
+        <S.WrapperStatusCount>
+          <div>
+            <h4>Followers</h4>
+            <span>5</span>
+          </div>
+          <div>
+            <h4>Starreds</h4>
+            <span>5</span>
+          </div>
+          <div>
+            <h4>Following</h4>
+            <span>5</span>
+          </div>
+        </S.WrapperStatusCount>
+      </S.WrapperInfoUser>
+    </S.Wrapper>
+  );
+};
+
+export default Profile;
