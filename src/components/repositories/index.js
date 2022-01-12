db420b48083e26c1c9d735f67cfec5fd0acfd46d
Marcos Moreira <marcosmoreira.r@gmail.com>
2022-01-12 14:49:19 -0300

    Segundo commit projeto Github App

---
+++ src/components/repositories/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import * as S from "./styled"
+
+const Repositories = () => {
+    return <S.WrapperTabs selectedTabClassName="is-selected" selectedTabPanelClassName="is-selected">
+        <S.WrapperTabList>
+            <S.WrapperTab>Repositores</S.WrapperTab>
+            <S.WrapperTab>Starred</S.WrapperTab>
+        </S.WrapperTabList>
+        <S.WrapperTabPanel>panel Repositories</S.WrapperTabPanel>
+        <S.WrapperTabPanel>panel Starred</S.WrapperTabPanel>
+    </S.WrapperTabs>
+}
+
+export default Repositories;
